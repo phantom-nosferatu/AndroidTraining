8e662619a75e85156a71e63f40b3ddbf3ccf3910
Admin <phantom.nosferatu13@gmail.com>
2022-04-03 15:06:31 +0300

    add viewmodel

--- Coroutines/app/src/main/java/com/example/coroutines/MainActivity.kt
+++ Coroutines/app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        val mainLayout : ConstraintLayout = findViewById(R.id.main_layout)
+        val tabs : TextView = findViewById(R.id.tv_taps)
+        val title : TextView = findViewById(R.id.tv_title)
+
+        mainLayout.setOnClickListener {
+            viewModel.updateTabs()
+        }
+
+        viewModel.tabs.observe(this) {
+            value ->
+            tabs.text = "$value tabs"
+        }
     }
+
 }
