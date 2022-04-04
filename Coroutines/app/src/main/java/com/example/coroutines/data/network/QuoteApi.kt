a84ee8406ae941f8aa6207c48c8cdf7546f3cd6c
Admin <phantom.nosferatu13@gmail.com>
2022-04-04 22:08:03 +0300

    create get-query

--- Coroutines/app/src/main/java/com/example/coroutines/data/network/QuoteApi.kt
+++ Coroutines/app/src/main/java/com/example/coroutines/data/network/QuoteApi.kt
@@ -1,5 +1,12 @@
 package com.example.coroutines.data.network
 
-class QuoteApi {
+import com.example.coroutines.data.network.model.Quote
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface QuoteApi {
+
+    @GET("/random")
+    suspend fun getQuote(): Response<Quote>
 
 }
