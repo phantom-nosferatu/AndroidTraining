4b4c4b29c587e1532378b0bd9a895065a09d5e13
Admin <phantom.nosferatu13@gmail.com>
2022-04-04 22:03:44 +0300

    add model class, base url

---
+++ Coroutines/app/src/main/java/com/example/coroutines/data/network/QuoteApi.kt
@@ -0,0 +1,5 @@
+package com.example.coroutines.data.network
+
+class QuoteApi {
+
+}
---
+++ Coroutines/app/src/main/java/com/example/coroutines/data/network/model/Quote.kt
@@ -0,0 +1,6 @@
+package com.example.coroutines.data.network.model
+
+data class Quote(
+    val _id: String,
+    val content: String
+)
